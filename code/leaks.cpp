045dc7e6b91edc4ce609ceebf16d36f67c58a2c7
Daniello <d_crush@hotmail.com>
2018-02-13 21:16:38 +0100

    Update

---
+++ code/leaks.cpp
@@ -0,0 +1,15 @@
+    void Map::EraseMapPoint(MapPoint *pMP) {
+        unique_lock<mutex> lock(mMutexMap);
+        mspMapPoints.erase(pMP);
+
+        // TODO: This only erase the pointer.
+        // Delete the MapPoint
+    }
+
+    void Map::EraseKeyFrame(KeyFrame *pKF) {
+        unique_lock<mutex> lock(mMutexMap);
+        mspKeyFrames.erase(pKF);
+
+        // TODO: This only erase the pointer.
+        // Delete the MapPoint
+    }
